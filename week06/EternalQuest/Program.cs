92a90c9d6d9bfcd3b67f5ee55380c67298b22f5d
lalathehung <1808arthur@gmail.com>
2025-02-17 03:48:35 -0400

    Updated

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic; // Required for List<Goal>
 using EternalQuest;
-
+//Creativity: I have added an achievement system, when the user reaches a certain level, the user will receive a certain title.
 class Program
 {
     static void Main(string[] args)
